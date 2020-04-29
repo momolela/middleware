d130e4ec86d7bc9c2efc8ffd8c530edc58bbf85c
momolela <1083910359@qq.com>
2020-04-30 00:30:43 +0800

    add activemq

---
+++ activemq/src/main/java/com/momolela/consumer/JmsConsumerQueue.java
@@ -0,0 +1,59 @@
+package com.momolela.consumer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+
+public class JmsConsumerQueue {
+
+    private static final String ACTIVEMQ_URL = "tcp://192.168.56.101:61616";
+    private static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws JMSException, IOException {
+        // 1、创建连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        // 2、创建连接并启动
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        // 3、创建会话，
+        // 第一个参数：事务
+        // 第二个参数：签收类型
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 4、创建目的地：queue
+        Queue queue = session.createQueue(QUEUE_NAME);
+        // 5、创建消费者
+        MessageConsumer messageConsumer = session.createConsumer(queue);
+
+        // 6、这里使用receive主动消费
+//        while (true) {
+//            TextMessage textMessage = (TextMessage) messageConsumer.receive(4000L);
+//            if (textMessage != null) {
+//                System.out.println("消费者收到的消息是：：：" + textMessage.getText());
+//            } else {
+//                break;
+//            }
+//        }
+
+        // 6、这里通过监听的方式进行消费
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                if (textMessage != null && textMessage instanceof TextMessage) {
+                    try {
+                        System.out.println("消费者收到的消息是：：：" + textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read(); // 保持程序不关闭，随意输入退出程序
+
+        // 7、关闭资源
+        messageConsumer.close();
+        session.close();
+        connection.close();
+    }
+}
---
+++ activemq/src/main/java/com/momolela/producer/JmsProducerQueue.java
@@ -0,0 +1,37 @@
+package com.momolela.producer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class JmsProducerQueue {
+
+    private static final String ACTIVEMQ_URL = "tcp://192.168.56.101:61616";
+    private static final String QUEUE_NAME = "queue01";
+
+    public static void main(String[] args) throws JMSException {
+        // 1、创建连接工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        // 2、创建连接并启动
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        // 3、创建会话，
+        // 第一个参数：事务
+        // 第二个参数：签收类型
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 4、创建目的地：queue
+        Queue queue = session.createQueue(QUEUE_NAME);
+        // 5、创建生产者对象
+        MessageProducer producer = session.createProducer(queue);
+        // 6、使用生产者对象发送消息到队列
+        for (int i = 1; i <= 3; i++) {
+            TextMessage textMessage = session.createTextMessage("发送第" + i + "条消息到MQ");
+            producer.send(textMessage);
+        }
+        // 7、关闭资源
+        producer.close();
+        session.close();
+        connection.close();
+        System.out.println("消息发布到MQ完成");
+    }
+}
